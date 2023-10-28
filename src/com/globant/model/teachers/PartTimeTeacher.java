6139322dba4998a045096fd8ba7b1a97ee05c74b
Ronald A. Vera C. <114179022+r4vc31@users.noreply.github.com>
2023-10-28 13:11:36 -0500

    Implement classes for the teachers section

--- src/com/globant/model/teachers/PartTimeTeacher.java
+++ src/com/globant/model/teachers/PartTimeTeacher.java
@@ -1,4 +1,21 @@
 package com.globant.model.teachers;
 
-public class PartTimeTeacher {
+public class PartTimeTeacher extends Teacher{
+    private int activeHoursPerWeek;
+    public PartTimeTeacher(String name, int activeHoursPerWeek) {
+        super(name);
+        this.activeHoursPerWeek = activeHoursPerWeek;
+    }
+
+    @Override
+    public double calculateSalary() {
+        this.setSalary(getBaseSalary() * activeHoursPerWeek);
+        return this.getSalary();
+    }
+
+    @Override
+    public String toString() {
+        return "Part Time " + super.toString() +
+                "\n\tWork Hours : " + activeHoursPerWeek + "pw";
+    }
 }
--- src/com/globant/model/teachers/Teacher.java
+++ src/com/globant/model/teachers/Teacher.java
@@ -1,4 +1,32 @@
 package com.globant.model.teachers;
 
-public class Teacher {
+public abstract class Teacher {
+    private String name;
+    private static final double baseSalary = 1020;
+    private double salary;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public static double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public abstract double calculateSalary();
+
+    @Override
+    public String toString() {
+        return "Teacher" +
+                "\n\tName  : " + name +
+                "\n\tSalary: " + String.format("%.2f", salary);
+    }
 }
