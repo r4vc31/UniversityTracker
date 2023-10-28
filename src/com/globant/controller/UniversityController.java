604a0a94913adbebe52459522af759ac1677e2f3
Ronald A. Vera C. <114179022+r4vc31@users.noreply.github.com>
2023-10-28 15:38:04 -0500

    Add data initializer method to UniversityController

--- src/com/globant/controller/UniversityController.java
+++ src/com/globant/controller/UniversityController.java
@@ -2,6 +2,8 @@ package com.globant.controller;
 
 import com.globant.model.courses.Course;
 import com.globant.model.students.Student;
+import com.globant.model.teachers.FullTimeTeacher;
+import com.globant.model.teachers.PartTimeTeacher;
 import com.globant.model.teachers.Teacher;
 
 import java.util.*;
@@ -48,6 +50,40 @@ public class UniversityController {
             String courseInfo = course.getName().toUpperCase() + " Course " + " - " +
                     "Clasroom: " + course.getClassroom();
             System.out.println(courseInfo);
+        }
+    }
+
+
+    private void initializeData() {
+        // Initialize teachers, students, and classes
+
+        // Add 2 full time teachers
+        teachers.add(new FullTimeTeacher("Alice", 10));
+        teachers.add(new FullTimeTeacher("Bob", 12));
+
+        // Add 2 part-time teachers
+        teachers.add(new PartTimeTeacher("Charlie",20));
+        teachers.add(new PartTimeTeacher("Dave",15));
+
+        // Add 6 students
+        String[] studentsName = {"Juan", "Ronald", "José", "Antony", "Helen", "Katy"};
+        for (int i = 0; i <= 6; i++) {
+            students.add(new Student(studentsName[i], 20 + i));
+        }
+
+        // Add 4 courses
+        String[] coursesName = {"OS Design", "DB Systems", "AI Basics", "ML Intro"};
+        for (int i = 1; i <= 4; i++) {
+            Course course = new Course(coursesName[i-1]);
+            course.assignClassroom("Room_" + (100+i));
+            course.setTeacher(teachers.get(i - 1));
+            Student randomStudent;
+            Random rand = new Random();
+            for (int j = 0; j < 3; j++) {
+                randomStudent = students.get(rand.nextInt(students.size()));
+                course.addStudent(randomStudent);
+            }
+            courses.add(course);
         }
     }
 }
